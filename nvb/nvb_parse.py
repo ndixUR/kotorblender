c037be5b8d58955366b80583f7f0b64f47fdbc14
ndixUR <32601018+ndixUR@users.noreply.github.com>
2017-05-13 11:56:19 -0700

    Added a measure of sanity to nvb_parse library by pulling all the parse N functions back into a single implementation that takes N as a parameter, retained all the specific N driver methods.

--- nvb/nvb_parse.py
+++ nvb/nvb_parse.py
@@ -1,52 +1,33 @@
 """TODO: DOC."""
 
+def _f(asciiBlock, floatList, numVals):
+    """Parse floats into a numVals tuple into floatList"""
+    l_float = float
+    for line in asciiBlock:
+        vals = []
+        for idx in range(0, numVals):
+            vals.append(l_float(line[idx]))
+        floatList.append(tuple(vals))
 
 def f1(asciiBlock, floatList):
     """Parse a series on floats into a list."""
-    # floatList = [float(l[0]) for l in asciiBlock]
-    l_float = float
-    for line in asciiBlock:
-        floatList.append(l_float(line[0]))
-
+    _f(asciiBlock, floatList, 1)
 
 def f2(asciiBlock, floatList):
     """Parse a series on float tuples into a list."""
-    # floatList = [(float(l[0]), float(l[1])) for l in asciiBlock]
-    l_float = float
-    for line in asciiBlock:
-        floatList.append((l_float(line[0]),
-                          l_float(line[1])))
-
+    _f(asciiBlock, floatList, 2)
 
 def f3(asciiBlock, floatList):
     """Parse a series on float 3-tuples into a list."""
-    l_float = float
-    for line in asciiBlock:
-        floatList.append((l_float(line[0]),
-                          l_float(line[1]),
-                          l_float(line[2])))
-
+    _f(asciiBlock, floatList, 3)
 
 def f4(asciiBlock, floatList):
     """Parse a series on float 4-tuples into a list."""
-    l_float = float
-    for line in asciiBlock:
-        floatList.append((l_float(line[0]),
-                          l_float(line[1]),
-                          l_float(line[2]),
-                          l_float(line[3])))
-
+    _f(asciiBlock, floatList, 4)
 
 def f5(asciiBlock, floatList):
     """Parse a series on float 5-tuples into a list."""
-    l_float = float
-    for line in asciiBlock:
-        floatList.append((l_float(line[0]),
-                          l_float(line[1]),
-                          l_float(line[2]),
-                          l_float(line[3]),
-                          l_float(line[4])))
-
+    _f(asciiBlock, floatList, 5)
 
 def txt(asciiBlock, txtBlock):
     """TODO: DOC."""
